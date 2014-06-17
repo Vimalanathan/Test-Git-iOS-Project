f57b31328c2b83746cafbd1e7186b63a9dc0ac77
Vimalanathan <vimalanathanayyanar@gmail.com>
2014-06-17 13:53:26 +0530

    added a test class

---
+++ veTest.h
@@ -0,0 +1,13 @@
+//
+//  veTest.h
+//  Git Test
+//
+//  Created by Vimalanathan on 17/06/14.
+//  Copyright (c) 2014 Vimal Enterprises. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface veTest : NSObject
+
+@end
---
+++ veTest.m
@@ -0,0 +1,13 @@
+//
+//  veTest.m
+//  Git Test
+//
+//  Created by Vimalanathan on 17/06/14.
+//  Copyright (c) 2014 Vimal Enterprises. All rights reserved.
+//
+
+#import "veTest.h"
+
+@implementation veTest
+
+@end
